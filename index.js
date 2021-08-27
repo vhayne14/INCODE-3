31db8fb3db847e6d0305393e11f0cc3e26b6ad31
vhayne14 <vhayne14@gmail.com>
2021-08-27 11:38:15 +1000

    Added view engine EJS, made two folders (public and views)

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const data = require('./data')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const msg = "Hello World Branch 3b"
 console.log(data.users)
 
+
+// Body Parser
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+// Set view engine EJS
+app.set('view engine', 'ejs')
+
+// Set our static folder
+app.use(express.static('public'))
+
 // GET request - route
+
+
 app.get('/',(req, res) =>{
     res.send(msg)
 })
