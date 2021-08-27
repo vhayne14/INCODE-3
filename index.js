762c824be64b9e5d45455ded0a43c15e1db03428
vhayne14 <vhayne14@gmail.com>
2021-08-27 15:05:49 +1000

    Added schedules.ejs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const data = require('./data')
 const app = express()
 const PORT = process.env.PORT || 3000
 const msg = "Hello World Branch 3b"
-console.log(data.users)
+// console.log(data.users)
 
 
 // Body Parser
@@ -13,15 +13,28 @@ app.use(express.urlencoded({extended:false}))
 // Set view engine EJS
 app.set('view engine', 'ejs')
 
+//app.set('views','./example')  Customized your views folder name to example
+
 // Set our static folder
 app.use(express.static('public'))
 
 // GET request - route
 
-
+// Homepage
 app.get('/',(req, res) =>{
-    res.send(msg)
+    res.render("pages/index")
 })
+
+// Displays all users
+app.get('/users',(req, res) =>{
+    res.render("pages/users", {
+        users: data.users
+    })
+    
+})
+
+
+
 
 app.listen(PORT, () => {
     console.log(`You guys are doing great, here's your app:
